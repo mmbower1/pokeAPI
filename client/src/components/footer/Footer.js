dc9a00d48e035b061d37736487231e553934e5e7
Matthew Bower <mttbwr91@gmail.com>
2020-08-13 20:29:58 -0700

    redo footer

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -5,10 +5,31 @@ import './Footer.scss';
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <p>https://pokeAPI.com, all rights reserved</p>
-      {/* <a href="mailto:hege@example.com">hege@example.com</a> */}
-    </footer>
+    <div className="footer">
+      <div className="footer__logo-box">
+        <img src={footerLogo} alt="Full logo" className="footer__logo" />
+      </div>
+      <div className="row">
+        <div className="col-1-of-2">
+          <div className="footer__navigation">
+            <ul className="footer__list">
+              <li className="footer__item">
+                <a href="" className="footer__link">Company</a>
+                <a href="" className="footer__link">Contact</a>
+                <a href="" className="footer__link">Careers</a>
+                <a href="" className="footer__link">Privacy policy</a>
+                <a href="" className="footer__link">Terms</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-1-of-2">
+          <p className="footer__copyright">
+            Copyright &copy; 2020 https://norcal-outdoors.com/
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
